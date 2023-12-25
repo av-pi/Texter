9f999c357979a796d5498aadb0819646fdd8dfd7
av-pi <akash.varma0110@gmail.com>
2023-12-26 03:55:22 +0530

    Added colours for custom light and dark themes in accordance with material design 3.

--- app/src/main/java/com/example/texter/ui/theme/Color.kt
+++ app/src/main/java/com/example/texter/ui/theme/Color.kt
@@ -2,10 +2,30 @@ package com.example.texter.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//val Purple80 = Color(0xFFD0BCFF)
+//val PurpleGrey80 = Color(0xFFCCC2DC)
+//val Pink80 = Color(0xFFEFB8C8)
+//
+//val Purple40 = Color(0xFF6650a4)
+//val PurpleGrey40 = Color(0xFF625b71)
+//val Pink40 = Color(0xFF7D5260)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark theme
+val InkyDepths = Color(0xFF1E3A8A) // Deep Navy Blue
+val MoonlightGlimmer = Color(0xFFF8F9F9) // Very Light Gray
+val PaleSeafoam = Color(0xFFD3F2EF) // Pale Seafoam
+val DarkNavyGray = Color(0xFF2C333A) // Dark Navy Gray
+val DarkBlueGray = Color(0xFF242A35) // Dark Blue Gray
+val BioluminescentTeal = Color(0xFF50B776) // Teal Green
+val DeepYellowGlow = Color(0xFFFCE0AD) // Deep Yellow
+
+// Light theme
+val SeaGlassGreen = Color(0xFFC2E5CE)
+val Charcoal = Color(0xFF212529)
+val LightOliveGreen = Color(0xFFE2EFD1)
+val LightBlueGray = Color(0xFFF3F5F2)
+val VeryLightLilac = Color(0xFFFCE0FF)
+
+// Helper colors
+val MutedGreen = Color(0xFFABDA9E)
+val ErrorRed = Color(0xFFD00036)
--- app/src/main/java/com/example/texter/ui/theme/Theme.kt
+++ app/src/main/java/com/example/texter/ui/theme/Theme.kt
@@ -10,21 +10,34 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = InkyDepths,
+    onPrimary = MoonlightGlimmer,
+    secondary = PaleSeafoam,
+    onSecondary = InkyDepths,
+    surface = DarkNavyGray,
+    background = DarkBlueGray,
+    onBackground = MoonlightGlimmer, // Ensure readability on dark backgrounds
+    tertiary = BioluminescentTeal,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = SeaGlassGreen,
+    onPrimary = Charcoal,
+    secondary = MoonlightGlimmer,
+    onSecondary = SeaGlassGreen,
+    surface = LightOliveGreen,
+    background = LightBlueGray,
+    onBackground = Charcoal,
+    error = ErrorRed,
+    onError = Color.White,
+    tertiary = VeryLightLilac,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -41,7 +54,7 @@ private val LightColorScheme = lightColorScheme(
 fun TexterTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
